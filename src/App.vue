b81b04cfc096f3093ff2c55d24fc910004cb2287
goji <>
2019-12-03 17:47:37 +0900

    方針変更　ボードに２次元配列もたす

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  height: 100%;
-  width: 100%;
 }
 </style>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,44 +1,134 @@
 <template>
-  <div class="board">
-    <div v-for="x in 9" :key="x" >
-      <div v-for="y in 9" :key="y" >
-        <div class="box">
-
-          <foot-soldier>
-            <template v-slot:pieces="pieces">
-              {{ pieces.pieceName }}
-            </template>
-          </foot-soldier>
-        </div>
-      </div>
-    </div>
-  </div>
+  <table class="board">
+    <tbody>
+      <tr v-for="x in 9" :key="x" >
+        <td v-for="y in 9" :key="y" >
+          <div class="box" @click="putPiece">
+            <div v-for="piece in pieceis" :key="piece.id">
+              <div v-if="piece.enemyPosition.includes((y * 10) + x) || piece.myPosition.includes((y * 10) + x)">
+                  <div v-if="piece.id==1">
+                    <pawn></pawn>
+                  </div>
+                  <div v-else-if="piece.id==2">
+                    香
+                  </div>
+                  <div v-else-if="piece.id==3">
+                    桂
+                  </div>
+                  <div v-else-if="piece.id==4">
+                    銀
+                  </div>
+                  <div v-else-if="piece.id==5">
+                    金
+                  </div>
+                  <div v-else-if="piece.id==6">
+                    角
+                  </div>
+                  <div v-else-if="piece.id==7">
+                    飛
+                  </div>
+                  <div v-else-if="piece.id==8">
+                    王
+                  </div>
+              </div>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import footSoldier from "./piece/footSoldier"
+import Pawn from "./piece/Pawn"
 
 export default {
   components: {
-    footSoldier
+    Pawn
   },
   data() {
     return {
-      board: [
-        ["香","桂","銀","金","王","金","銀","桂","香"],
-        ["　","角","　","　","　","　","　","飛","　"],
-        ["歩","歩","歩","歩","歩","歩","歩","歩","歩"],
-        ["　","　","　","　","　","　","　","　","　"],
-        ["　","　","　","　","　","　","　","　","　"],
-        ["　","　","　","　","　","　","　","　","　"],
-        ["歩","歩","歩","歩","歩","歩","歩","歩","歩"],
-        ["　","角","　","　","　","　","　","飛","　"],
-        ["香","桂","銀","金","王","金","銀","桂","香"]
-      ],
-      choicedBoard: ""
+      pieceis: {
+        pawn: {
+          id: 1,
+          enemyPosition: [
+            13, 23, 33, 43, 53, 63, 73, 83, 93
+          ],
+          myPosition: [
+            17, 27, 37, 47, 57, 67, 77, 87, 97
+          ]
+        },
+        lance: {
+          id: 2,
+          enemyPosition: [
+            11, 91
+          ],
+          myPosition: [
+            19, 99
+          ]
+        },
+        knight: {
+          id: 3,
+          enemyPosition: [
+            21, 81
+          ],
+          myPosition: [
+            29, 89
+          ]
+        },
+        silverGeneral: {
+          id: 4,
+          enemyPosition: [
+            31, 71
+          ],
+          myPosition: [
+            39, 79
+          ]
+        },
+        goldGeneral: {
+          id: 5,
+          enemyPosition: [
+            41, 61
+          ],
+          myPosition: [
+            49, 69
+          ]
+        },
+        bishop: {
+          id: 6,
+          enemyPosition: [
+            82
+          ],
+          myPosition: [
+            28
+          ]
+        },
+        rook: {
+          id: 7,
+          enemyPosition: [
+            22,
+          ],
+          myPosition: [
+            88
+          ]
+        },
+        king: {
+          id: 8,
+          enemyPosition: [
+            51
+          ],
+          myPosition: [
+            59
+          ]
+        }
+      },
+      board: [9][9]
     }
   },
   methods: {
+    putPiece() {
+
+    }
   },
   computed: {
   },
@@ -47,11 +137,12 @@ export default {
 
 <style scoped>
 .board {
-  display: grid;
-  grid-template-rows: repeat(9, 1fr);
-  grid-template-columns: repeat(9, 1fr);
+  font-size: 30px;
 }
 .box {
+  margin: 0;
+  width: 60px;
+  height: 60px;
   border: 1px solid black;
 }
 </style>
