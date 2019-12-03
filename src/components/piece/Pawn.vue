c4a5753d5b3c9854545795635b035f4ee4bc393a
goji <>
2019-12-03 17:47:52 +0900

    no message

---
+++ src/components/piece/Pawn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ pieceName }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pieceName: '歩',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
